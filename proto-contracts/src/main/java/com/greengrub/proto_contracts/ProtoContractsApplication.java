43f0a7cc0bd43f869c4cbae83b7f8a761bfc2c52
I528797 <subhadip.jana@sap.com>
2026-03-15 11:54:39 +0530

    Initial commit

---
+++ proto-contracts/src/main/java/com/greengrub/proto_contracts/ProtoContractsApplication.java
@@ -0,0 +1,13 @@
+package com.greengrub.proto_contracts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProtoContractsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProtoContractsApplication.class, args);
+	}
+
+}
---
+++ proto-contracts/src/test/java/com/greengrub/proto_contracts/ProtoContractsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greengrub.proto_contracts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProtoContractsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
