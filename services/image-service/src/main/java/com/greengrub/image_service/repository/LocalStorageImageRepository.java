8523289dc8134ebff2489a72306764b1e40c7afd
I528797 <subhadip.jana@sap.com>
2026-06-02 11:44:00 +0530

    increasing k8s resource limit

--- services/image-service/src/main/java/com/greengrub/image_service/repository/LocalStorageImageRepository.java
+++ services/image-service/src/main/java/com/greengrub/image_service/repository/LocalStorageImageRepository.java
@@ -1,10 +1,12 @@
 package com.greengrub.image_service.repository;
 
 import com.greengrub.image_service.entity.LocalImage;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import java.util.List;
 
+@Profile("local")
 public interface LocalStorageImageRepository extends MongoRepository<LocalImage, String> {
     List<LocalImage> findByCreatorId(String creatorId);
     boolean deleteByImageId(String imageId);
